ff5e3bf825d3c76628a193d41887023452a44721
webdev-sj <125741103+webdev-sj@users.noreply.github.com>
2024-06-12 21:42:32 +0100

    no change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 import AcmeLogo from '@/app/ui/acme-logo';
 
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+// import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
@@ -18,20 +18,20 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           
-          {/* <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}> */}
-          <p
-      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-    >
-            Transforming ideas into reality through code, design, and illustration. 
-           
-          </p>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            Transforming ideas into reality through code, design, and illustration.</p>
+
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Log in</span> 
           </Link>
+
         </div>
+
+
+
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -5,11 +5,16 @@ import logo from '../ui/logo.svg';
 export default function SJLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-black`}
     >
-            <Image src={logo} alt="Sam Jarrah Logo" className="h-12 w-12 rotate-[0deg]" />
+            <Image src={logo} alt="Sam Jarrah Logo" className="h-10 w-10 rotate-[0deg]" />
+            <div className="text-[35px] ">SamJ</div>
 
-      <p className="text-[44px]">SJ</p>
+     {/* <div className='flex flex-row items-center gap-5 uppercase'>
+     <p className="text-[30px] ">Designer</p>
+      <p className="text-[30px]">Developer</p>
+      <p className="text-[30px]">Illustrator</p>
+     </div> */}
     </div>
   );
 }
